acfbdaad22336e7d2c261fcfaca37a591fc1c818
Jorge <jorge.l.lopez38@gmail.com>
2023-05-06 11:33:05 -0500

    adding exercises

--- Revising_aggregate_query.sql
+++ Revising_aggregate_query.sql
@@ -65,3 +65,71 @@ GROUP BY EARNINGS DESC
 LIMIT 1;
 
 ###############################################################################################
+
+-- Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than 38.7880 and less than 137.2345. 
+-- Truncate your answer to 4 decimal places.
+
+SELECT ROUND(SUM(LAT_N),4) AS SUM_LAT
+FROM STATION
+WHERE LAT_N < 137.2345 AND LAT_N > 38.7880
+
+###############################################################################################
+
+-- Query the greatest value of the Northern Latitudes (LAT_N) from STATION that is less than 137.2345. 
+-- Truncate your answer to 4 decimal places.
+
+SELECT ROUND(MAX(LAT_N),4) AS SUM_LAT
+FROM STATION
+WHERE LAT_N < 137.2345;
+
+###############################################################################################
+
+-- Query the Western Longitude (LONG_W) for the largest Northern Latitude (LAT_N) in STATION that is less than . 
+-- Round your answer to 4 decimal places.
+
+SELECT ROUND(LONG_W,4) AS MEAN_LONG_W
+FROM STATION
+WHERE LAT_N < 137.2345
+ORDER BY LAT_N DESC
+LIMIT 1;
+
+###############################################################################################
+
+-- Query the smallest Northern Latitude (LAT_N) from STATION that is greater than 38.7780. Round your answer to 4 decimal places.
+
+SELECT ROUND(MIN(LAT_N),4) AS SMALL_LONG_W
+FROM STATION
+WHERE LAT_N > 38.7780;
+
+###############################################################################################
+
+-- Query the Western Longitude (LONG_W)where the smallest Northern Latitude (LAT_N) in 
+-- STATION is greater than 38.7780. Round your answer to 4 decimal places.
+
+SELECT ROUND(LONG_W,4) AS NEW_LONG_W
+FROM STATION
+WHERE LAT_N > 38.7780
+ORDER BY LAT_N
+LIMIT 1;
+
+###############################################################################################
+
+-- Consider P1(a,b) and P2(c,d) to be two points on a 2D plane.
+
+--  a happens to equal the minimum value in Northern Latitude (LAT_N in STATION).
+--  b happens to equal the minimum value in Western Longitude (LONG_W in STATION).
+--  c happens to equal the maximum value in Northern Latitude (LAT_N in STATION).
+--  d happens to equal the maximum value in Western Longitude (LONG_W in STATION).
+-- Query the Manhattan Distance between points P1 and P2 and round it to a scale of  decimal places.
+
+SELECT ROUND((MAX(LAT_N)- MIN(LAT_N)) + (MAX(LONG_W) - MIN(LONG_W)),4) AS MANHATTAN_DISTANCE
+FROM STATION;
+
+###############################################################################################
+
+-- Consider P1(a,c) and P2(b,d) to be two points on a 2D plane where (a,b) are the respective minimum and maximum values of Northern Latitude (LAT_N) 
+-- and (c,d) are the respective minimum and maximum values of Western Longitude (LONG_W) in STATION.
+-- Query the Euclidean Distance between points P1 and P2 and format your answer to display 4 decimal digits.
+
+SELECT ROUND(SQRT(POWER(MAX(LAT_N)- MIN(LAT_N), 2) + POWER(MAX(LONG_W) - MIN(LONG_W),2)),4) AS EUCLIDEAN_DISTANCE
+FROM STATION;
