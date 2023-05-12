fd915238689212922d7dfad7ec11703162299e57
Jorge <jorge.l.lopez38@gmail.com>
2023-05-12 16:06:46 -0500

    updates

--- Revising_aggregate_query.sql
+++ Revising_aggregate_query.sql
@@ -133,3 +133,12 @@ FROM STATION;
 
 SELECT ROUND(SQRT(POWER(MAX(LAT_N)- MIN(LAT_N), 2) + POWER(MAX(LONG_W) - MIN(LONG_W),2)),4) AS EUCLIDEAN_DISTANCE
 FROM STATION;
+
+###############################################################################################
+
+-- Query the following two values from the STATION table:
+-- The sum of all values in LAT_N rounded to a scale of 2 decimal places.
+-- The sum of all values in LONG_W rounded to a scale of 2 decimal places.
+
+SELECT ROUND(SUM(LAT_N),2), ROUND(SUM(LONG_W),2)
+FROM STATION;
